1f8d0f872642925a012a186b46a396f4d0423e5b
juan18506 <juan185067@gmail.com>
2023-08-24 16:15:24 -0300

    Initial commit

---
+++ js/almacenar.js
@@ -0,0 +1,31 @@
+const agregar = document.getElementById('agregar')
+const limpiar = document.getElementById('limpiar')
+const item = document.getElementById('item')
+const ul = document.getElementById('contenedor')
+
+const listaOnline = window.localStorage.getItem('lista')
+let lista = listaOnline ? JSON.parse(listaOnline) : []
+
+lista.forEach(item => {
+  ul.innerHTML += `
+    <li class="list-group-item">${item}</li>
+  `
+})
+
+agregar.addEventListener('click', () => {
+  const newValue = item.value
+  if (!newValue) return 
+  
+  lista.push(newValue)
+  window.localStorage.setItem('lista', JSON.stringify(lista))
+
+  ul.innerHTML += `
+    <li class="list-group-item">${newValue}</li>
+  `
+})
+
+limpiar.addEventListener('click', () => {
+  window.localStorage.removeItem('lista')
+  lista = []
+  ul.innerHTML = ''
+})
